6ef5077b37fe386d5a72a554c50b135fbee5597e
Paul Robertson <paul@our-pond.org.uk>
2015-04-12 15:58:53 +0100

    First bit with some html

--- castle/views.py
+++ castle/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from castle.models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.db import transaction
+from django.db.models import Sum
 
 # Create your views here.
+#-----------------------------------------------------------------------------
+# Views
+#-----------------------------------------------------------------------------
+def home(request):
+    context = { 'thing_list': 'moof' }
+    return render(request, 'castle/index.html', context)
