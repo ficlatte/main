13729bdd00905bb850b3d31818ab48999c56bde7
Paul Robertson <paul@our-pond.org.uk>
2017-01-11 14:25:58 +0000

    Added example settings file: you'll need to modify this to support your own development environment

---
+++ ficlatte/settings-example.py
@@ -0,0 +1,106 @@
+"""
+Django settings for ficlatte project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# DO NOT DISTRIBUTE THIS FILE!!
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'yxr5rs5bn#cbwf(kmo334drq#@16h5*^6x$09eml!tb=8el^s)'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'castle',
+    'bbcode',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'ficlatte.urls'
+
+WSGI_APPLICATION = 'ficlatte.wsgi.application'
+
+# Filesystem locations
+#TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ficlatte',
+        'USER' : 'ficlatte',
+        'PASSWORD' : 'your database password',
+        'HOST' : 'localhost',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Stuff
+LOGIN_URL = '/login/'
+SITE_URL = 'http://127.0.0.1:8002'
+SERVER_LOCATION = 'dev'
+DB = 'mysql'
+EMAIL_HOST = 'email-relay.example.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'email-relay-username'
+EMAIL_HOST_PASSWORD = 'email relay password'
+EMAIL_USE_TLS = True
+
+AVATAR_PATH='/home/ficlatteuser/django/ficlatte/castle/static/img'
