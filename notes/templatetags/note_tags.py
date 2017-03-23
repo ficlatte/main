a4d4dd5b2d7b0af89888b994c1068946e13369a0
Jim Stitzel <stitzelj@gmail.com>
2017-03-22 20:50:23 -0400

    Update for Notes bubble

--- notes/templatetags/note_tags.py
+++ notes/templatetags/note_tags.py
@@ -10,20 +10,23 @@ register = Library()
 #-----------------------------------------------------------------------------
 @register.filter
 def note_link(note):
-	if note is None:
-		return u'<NULL NOTE>'
-	if note.read_date is None:
-		return mark_safe(u'<b><a href="/notes/view/' + unicode(note.id) + u'" class="note-link">' + escape(note.subject) + u'</a></b>')
-	else:
-		return mark_safe(u'<a href="/notes/view/' + unicode(note.id) + u'" class="note-link">' + escape(note.subject) + u'</a>')
+    if note is None:
+        return u'<NULL NOTE>'
+    if note.read_date is None:
+        return mark_safe(u'<b><a href="/notes/view/' + unicode(note.id) + u'" class="note-link">' + escape(note.subject) + u'</a></b>')
+    else:
+        return mark_safe(u'<a href="/notes/view/' + unicode(note.id) + u'" class="note-link">' + escape(note.subject) + u'</a>')
 
 #-----------------------------------------------------------------------------
 @register.filter
 def inbox_count(profile):
-	count = Note.objects.filter(recipient=profile, read_date__isnull=True, recipient_deleted_date__isnull=True).count()
-	return mark_safe(u'<span class="inbox-count">' + escape(count) + u'</span>')
+    count = Note.objects.filter(recipient=profile, read_date__isnull=True, recipient_deleted_date__isnull=True).count()
+    if count > 0:
+        return mark_safe(u'<span class="inbox-count">' + escape(count) + u'</span>')
+    else:
+        return mark_safe(u'<span class="inbox-zero">' + escape(count) + u'</span>')
 
 #-----------------------------------------------------------------------------
 @register.filter
 def author_msg(profile):
-	return mark_safe(u'<a class="btn btn-success author-msg-btn" href="/notes/compose?recipient=' + escape(profile.pen_name) + u'" type="button"><span class="glyphicon glyphicon-pencil"></span> Message ' + escape(profile.pen_name) + u'</a>')
+    return mark_safe(u'<a class="btn btn-success author-msg-btn" href="/notes/compose?recipient=' + escape(profile.pen_name) + u'" type="button"><span class="glyphicon glyphicon-pencil"></span> Message ' + escape(profile.pen_name) + u'</a>')
